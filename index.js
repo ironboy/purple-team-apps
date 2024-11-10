82ae9b71e98d7a6b49dd63ea92eaa9421d5257bc
Ironboy <ironboy@users.noreply.github.com>
2024-11-10 11:53:19 +0100

    change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+const port = 3001;
 const app = express();
 app.get('*', (_req, res) => res.send('Hello world!'));
-app.listen(3001, () => console.log('Listening on http://localhost:80'));
+app.listen(port, () => console.log('Listening on http://localhost:' + port));
