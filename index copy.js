e82a16dfa3190267a2d7c37eaf04a82d7c5e913f
Ironboy <ironboy@users.noreply.github.com>
2024-11-15 14:21:54 +0100

    photos

--- index copy.js
+++ index copy.js
@@ -6,13 +6,15 @@ const port = 3002;
 const app = express();
 app.use(express.static('frontend'));
 
-app.get('/api/photolist', (req, res) => {
-  const list = fs.readdirSync(path.join(import.meta.dirname, 'images', req.query.folder));
+app.get('/api/photolist/*', (req, res) => {
+  const directory = req.url.split('photolist/')[1];
+  const list = fs.readdirSync('/' + directory, { recursive: true });
   res.json(list);
 });
 
-app.get('/api/photo', (req, res) => {
-  res.sendFile(path.join(import.meta.dirname, 'images', req.query.file));
+app.get('/api/photo/*', (req, res) => {
+  const photo = req.url.split('photo/')[1];
+  res.sendFile('/' + photo);
 });
 
 app.get('*', ((_req, res) =>
