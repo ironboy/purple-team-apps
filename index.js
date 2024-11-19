1362528029af8cfb2a3508d12cd4c130c7670326
Ironboy <ironboy@users.noreply.github.com>
2024-11-19 07:49:59 +0100

    Secure part 1

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const app = express();
 app.use(express.static('frontend'));
 
 app.get('/api/photolist', (req, res) => {
-  const list = fs.readdirSync(path.join(import.meta.dirname, 'images', req.query.folder));
+  const folder = req.query.folder.replaceAll('.', '');
+  const list = fs.readdirSync(path.join(import.meta.dirname, 'images', folder));
   res.json(list);
 });
 
 app.get('/api/photo', (req, res) => {
-  res.sendFile(path.join(import.meta.dirname, 'images', req.query.file));
+  const file = req.query.file.replaceAll('.', '');
+  res.sendFile(path.join(import.meta.dirname, 'images', file));
 });
 
 app.get('*', ((_req, res) =>
   res.sendFile(path.join(import.meta.dirname, 'frontend', 'index.html'))));
-
 
 app.listen(port, () => console.log('Listening on http://localhost:' + port));
