2e600d5d7e5dbe21d203fcc015d7ea0304b35dc3
Ironboy <ironboy@users.noreply.github.com>
2024-11-28 12:54:23 +0100

    port 3004

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const betterSqlite3 = require('better-sqlite3');
 const db = betterSqlite3('./database/bookshop.db');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 const express = require('express');
 
 const app = express();
